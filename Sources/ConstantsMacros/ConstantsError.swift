a4ed23defccbfb513f5316066db9691080d22dc1
Nitin Kumar <nikumar@griddynamics.com>
2024-12-27 17:49:28 +0530

    worked on nagative case

--- Sources/ConstantsMacros/ConstantsError.swift
+++ Sources/ConstantsMacros/ConstantsError.swift
@@ -13,6 +13,8 @@ enum ConstantsError: Error, CustomStringConvertible {
     case invalidArgumentCount
     case invalidFormat
     case notADictionary
+    case emptyDictionary
+    case emptyKey
     case unknown
     
     var description: String {
@@ -23,6 +25,10 @@ enum ConstantsError: Error, CustomStringConvertible {
             return "This is not a valid format; 'struct' name is missing."
         case .notADictionary:
             return "The macro's arguments must be a dictionary."
+        case .emptyDictionary:
+            return "The dictionary must not be empty"
+        case .emptyKey:
+            return "The key must not be empty"
         case .unknown:
             return "Unknown error occurred."
         }
